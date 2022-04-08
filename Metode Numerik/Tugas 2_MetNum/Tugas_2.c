06a2829a8a78616853bfd86276e349a4eb3e49e1
bills <221911069@stis.ac.id>
2022-04-08 22:41:39 +0700

    Added Main Menu

--- Metode Numerik/Tugas 2_MetNum/Tugas_2.c
+++ Metode Numerik/Tugas 2_MetNum/Tugas_2.c
@@ -53,6 +53,7 @@ int Biseksi(float coeff[], int deg) {
                 result_x = Polynomial(coeff, deg, x);
                 printf("|%d.| |%.10f|, |%.10f|, |%.10f|, |%.10f|, |%.10f|, |%.10f  |, |%.10f|\n", 
                         i, a, b, (a + b)/2, result_a, result_b, Polynomial(coeff, deg, (a + b)/2), err);
+                err = fabs(b - a);
                 if (result_x * result_a > 0) {
                     a = x;
                     result_a = result_x;
@@ -82,7 +83,7 @@ int IterasiSederhana(float coeff[], int deg) {
     scanf("%d", &N);
 
     int i = 1;
-    printf("|No|, |x_n|, \t\t\t|F(x_n)|\n");
+    printf("|No|, |x_n|, \t|F(x_n)|\n");
     while(i < N) {
         result_xn = Polynomial(coeff, deg, x);
         printf("|%d.| |%.10f|, |%.10f|\n", 
@@ -167,7 +168,7 @@ int NewtonRaphson(float coeff[], int deg) {
     int N;
     float x_n, x_nplus1, TOL;
 
-    printf("Masukan nilai X_n: ");
+    printf("Masukan nilai x_n: ");
     scanf("%f", &x_n);
     printf("Masukan jumlah iterasi: ");
     scanf("%d", &N);
@@ -179,31 +180,221 @@ int NewtonRaphson(float coeff[], int deg) {
         x_nplus1 = x_n - Polynomial(coeff, deg, x_n) / Derivative(coeff, deg, x_n);
         printf("|%d.| |%.10f|\n", 
                 i, x_n);
-        if (fabs(x_nplus1 - x_n) < TOL)
-            break;
         x_n = x_nplus1;
         i++;
     }
 }
 
 int main(){
+    system("cls");
     float x, coeff[100], y1;
-    int deg, i;
+    int deg, j, no_menu;
+    char next, stay;
+    
+    Utama:
+    printf("SELAMAT DATANG DI APLIKASI ALGORITMA PENCARIAN SOLUSI DARI POLINOM\n\n");
+    printf("Pilih menu yang tersedia:\n");
+    printf("1. Metode Biseksi\n");
+    printf("2. Metode Iterasi Sederhana\n");
+    printf("3. Metode Iterasi Aitken\n");
+    printf("4. Metode Regula Falsi\n");
+    printf("5. Metode Newton Raphson\n");
+    printf("6. Keluar\n\n");
+    printf("Masukan nomor menu: ");
+    scanf("%d", &no_menu);
+    system("cls");
 
-    printf("Enter the degree of polynomial equation: ");
-    scanf("%d", &deg);
+    switch (no_menu) {
+        case 1:
+            printf("ALGORITMA BISEKS\n\n");
+            printf("Ketentuan dari algoritma ini antara lain:\n");
+            printf("1. User memasukan langsung nilai batas bawah (a) dan batas atas (b);\n");
+            printf("2. User memasukan nilai toleransi (TOL) secara manual;\n");
+            printf("3. User memasukan nilai iterasi (N); dan\n");
+            printf("4. User memasukan secara manual fungsi polinomial yang ingin diketahui solusinya.\n\n");
+            printf("Untuk contoh implementasi, silahkan gunakan polinomial berikut: x^3 + x^2 - 3x - 3\n");
+            printf("Masukan nilai a: 1, nilai b: 2\n");
+            printf("Masukan nilai TOL: 0.0001\n");
+            printf("Silahkan diingat input polinom, a, b, dan TOL nya.\n\n");
 
-    for (i = 0; i <= deg; i++) {
-        printf("Enter the coefficient of x to the power %d: ", i);
-        scanf("%f", &coeff[i]);
+            printf("Lanjut ke implementasi? (y/n): ");
+            scanf(" %c", &next);
+            if (next == 'y') {
+                system("cls");
+                Biseksi:
+                printf("Masukan derajat polinom: ");
+                scanf("%d", &deg);
+                for (j = 0; j <= deg; j++) {
+                    printf("Masukan nilai koefisien setiap x pangkat %d: ", j);
+                    scanf("%f", &coeff[j]);
+                }
+
+                Biseksi(coeff, deg);
+
+                printf("Mau ulangi? (y/n): ");
+                scanf(" %c", &stay);
+                if (stay == 'y')
+                    goto Biseksi;
+                else
+                    goto Utama;
+            } else {
+                goto Utama;
+            }
+            break;
+
+        case 2:
+            printf("ALGORITMA ITERASI SEDERHANA\n\n");
+            printf("Ketentuan dari algoritma ini antara lain:\n");
+            printf("1. User memasukan secara manual nilai x awal;\n");
+            printf("2. User memasukan nilai toleransi (TOL) secara manual;\n");
+            printf("3. User memasukan nilai iterasi (N); dan\n");
+            printf("4. User memasukan secara manual fungsi polinomial yang ingin diketahui solusinya.\n\n");
+            printf("Untuk contoh implementasi, silahkan gunakan polinomial berikut: 2/7(x^3 - 1)\n");
+            printf("Masukan nilai x awal: 1\n");
+            printf("Silahkan diingat input polinom dan x nya.\n\n");
+            printf("NOTE: Jika koefisien polinomial berbentuk pecahan, jangan lupa mengubahnya menjadi bilangan desimal\ndengan angka signifikan yang sesuai dengan ketentuan pada soal.");
+            
+            printf("\nLanjut ke implementasi? (y/n): ");
+            scanf(" %c", &next);
+            if (next == 'y') {
+                system("cls");
+                IterasiSederhana:
+                printf("Masukan derajat polinom: ");
+                scanf("%d", &deg);
+                for (j = 0; j <= deg; j++) {
+                    printf("Masukan nilai koefisien setiap x pangkat %d: ", j);
+                    scanf("%f", &coeff[j]);
+                }
+
+                IterasiSederhana(coeff, deg);
+
+                printf("Mau ulangi? (y/n): ");
+                scanf(" %c", &stay);
+                if (stay == 'y')
+                    goto IterasiSederhana;
+                else
+                    goto Utama;
+            } else {
+                goto Utama;
+            }
+            break;
+
+        case 3:
+            printf("ALGORITMA ITERASI AITKEN\n\n");
+            printf("Ketentuan dari algoritma ini antara lain:\n");
+            printf("1. User memasukan secara manual nilai x_0;\n");
+            printf("2. User memasukan secara manual jumlah iterasi (N);\n");
+            printf("3. User memasukan secara manual derajat polinomial; dan\n");
+            printf("4. User memasukan secara manual fungsi polinomial yang ingin diketahui solusinya.\n\n");
+            printf("Untuk contoh implementasi, silahkan gunakan polinomial berikut: (x^2 + 8)/6\n");
+            printf("Masukan nilai x_0: 1\n");
+            printf("Masukan jumlah iterasi: 4\n");
+            printf("Silahkan diingat input polinom dan x nya.\n\n");
+            printf("NOTE: Jika koefisien polinomial berbentuk pecahan, jangan lupa mengubahnya menjadi bilangan desimal\ndengan angka signifikan yang sesuai dengan ketentuan pada soal.");
+            
+            printf("\nLanjut ke implementasi? (y/n): ");
+            scanf(" %c", &next);
+            if (next == 'y') {
+                system("cls");
+                IterasiAitken:
+                printf("Masukan derajat polinom: ");
+                scanf("%d", &deg);
+                for (j = 0; j <= deg; j++) {
+                    printf("Masukan nilai koefisien setiap x pangkat %d: ", j);
+                    scanf("%f", &coeff[j]);
+                }
+
+                IterasiAitken(coeff, deg);
+
+                printf("Mau ulangi? (y/n): ");
+                scanf(" %c", &stay);
+                if (stay == 'y')
+                    goto IterasiAitken;
+                else
+                    goto Utama;
+            } else {
+                goto Utama;
+            }
+            break;
+
+        case 4:
+            printf("ALGORITMA REGULA FALSI\n\n");
+            printf("Ketentuan dari algoritma ini antara lain:\n");
+            printf("1. User memasukan secara manual nilai x_n;\n");
+            printf("2. User memasukan secara manual nilai x_n+1;\n");
+            printf("3. User memasukan secara manual jumlah iterasi (N);\n");
+            printf("4. User memasukan secara manual nilai error (EPSILON); dan\n");
+            printf("5. User memasukan secara manual fungsi polinomial yang ingin diketahui solusinya.\n");
+            printf("Untuk contoh implementasi, silahkan gunakan polinomial berikut: x^3 - 7x + 1\n");
+            printf("Masukan nilai x_n: 2.5, x_n+1: 2.6\n");
+            printf("Silahkan diingat input polinom, x_n, dan x_n+1 nya.\n\n");
+            printf("NOTE: Jika koefisien polinomial berbentuk pecahan, jangan lupa mengubahnya menjadi bilangan desimal\ndengan angka signifikan yang sesuai dengan ketentuan pada soal.");
+            
+            printf("\nLanjut ke implementasi? (y/n): ");
+            scanf(" %c", &next);
+            if (next == 'y') {
+                system("cls");
+                RegulaFalsi:
+                printf("Masukan derajat polinom: ");
+                scanf("%d", &deg);
+                for (j = 0; j <= deg; j++) {
+                    printf("Masukan nilai koefisien setiap x pangkat %d: ", j);
+                    scanf("%f", &coeff[j]);
+                }
+
+                RegulaFalsi(coeff, deg);
+
+                printf("Mau ulangi? (y/n): ");
+                scanf(" %c", &stay);
+                if (stay == 'y')
+                    goto RegulaFalsi;
+                else
+                    goto Utama;
+            } else {
+                goto Utama;
+            }
+            break;
+
+        case 5:
+            printf("ALGORITMA NEWTON-RAPHSON\n\n");
+            printf("Ketentuan dari algoritma ini antara lain:\n");
+            printf("1. User memasukan secara manual nilai x_n;\n");
+            printf("2. User memasukan secara manual jumlah iterasi (N);\n");
+            printf("3. User memasukan secara manual derajat polinomial; dan\n");
+            printf("4. User memasukan secara manual fungsi polinomial yang ingin diketahui solusinya.\n\n");
+            printf("Untuk contoh implementasi, silahkan gunakan polinomial berikut: x^3 - x - 1\n");
+            printf("Masukan nilai x_n: 1.5\n");
+            printf("Silahkan diingat input polinom dan x_n nya.\n\n");
+            printf("NOTE: Jika koefisien polinomial berbentuk pecahan, jangan lupa mengubahnya menjadi bilangan desimal\ndengan angka signifikan yang sesuai dengan ketentuan pada soal.");
+            
+            printf("\nLanjut ke implementasi? (y/n): ");
+            scanf(" %c", &next);
+            if (next == 'y') {
+                system("cls");
+                NewtonRaphson:
+                printf("Masukan derajat polinom: ");
+                scanf("%d", &deg);
+                for (j = 0; j <= deg; j++) {
+                    printf("Masukan nilai koefisien setiap x pangkat %d: ", j);
+                    scanf("%f", &coeff[j]);
+                }
+
+                NewtonRaphson(coeff, deg);
+
+                printf("Mau ulangi? (y/n): ");
+                scanf(" %c", &stay);
+                if (stay == 'y')
+                    goto NewtonRaphson;
+                else
+                    goto Utama;
+            } else {
+                goto Utama;
+            }
+
+        case 6:
+            exit(0);
+
     }
-
-    // Biseksi(coeff, deg);
-    // IterasiSederhana(coeff, deg);
-    // IterasiAitken(coeff, deg);
-    // RegulaFalsi(coeff, deg);
-    // NewtonRaphson(coeff, deg);
-
     return 0;
 }
 
