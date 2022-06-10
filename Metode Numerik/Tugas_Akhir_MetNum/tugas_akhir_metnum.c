779cf4e3d0e5c1729c10c3646fcd69f3c9435db3
bills <221911069@stis.ac.id>
2022-06-10 22:46:22 +0700

    Added some changes

--- Metode Numerik/Tugas_Akhir_MetNum/tugas_akhir_metnum.c
+++ Metode Numerik/Tugas_Akhir_MetNum/tugas_akhir_metnum.c
@@ -5,21 +5,21 @@ float LagrangeInterpolation(float x[], float f_x[], int n, float x_interpolate)
 {
       float f_interpolate = 0;
       int i, j, d;
-      printf("\n\nMasukan jumlah data: ");
+      printf("Masukan jumlah data: ");
       scanf("%d",&n);
-      printf("\n\nMasukan nilai dari x dan f(x): \n");
+      printf("\n\nMasukan titik kolokasi x dan hasil fungsi f(x) (dipisah dengan Spasi): \n");
       for(i=0; i< n; i++)
       {
             scanf ("%f",&x[i]);
             scanf("%f",&f_x[i]);
       }
-      printf("\n\nThe table you entered is as follows:\n\n");
+      printf("\n\nBerikut parameter x dan f(x) yang diinput:\n\n");
       for(i=0; i< n; i++)
       {
-            printf("%0.7f\t%0.7f",x[i],f_x[i]);
+            printf("%0.4f\t%0.7f",x[i],f_x[i]);
             printf("\n");
       }
-      printf("Enter the value of the x to find the respective value of y: ");
+      printf("Masukan nilai x yang akan diestimasi nilai f(x)-nya: ");
       scanf("%f",&x_interpolate);
       for (i = 0; i < n; i++)
       {
@@ -33,12 +33,53 @@ float LagrangeInterpolation(float x[], float f_x[], int n, float x_interpolate)
             }
             f_interpolate += L * f_x[i];
       }
-      return f_interpolate;
+      printf("\nHasil estimasi dari titik %0.3f: %0.7f", x_interpolate, f_interpolate);
 }
 
 int main(){
       float x[100], f_x[100], x_inter;
-      int n;
-      printf("Aproximasi nilai: %0.7f", LagrangeInterpolation(x, f_x, n, x_inter));
+      int n, num_menu;
+      char next, stay;
+
+      Utama:
+      system("cls");
+      printf("APLIKASI APROKSIMASI FUNGSI, TURUNAN, DAN INTEGRAL DENGAN METODE NUMERIK\n");
+      printf("\nPilih menu yang tersedia:\n\n");
+      printf("1. Aproksimasi Fungsi dengan Metode Numerik\n");
+      printf("2. Aproksimasi Turunan Fungsi dengan Metode Numerik\n");
+      printf("3. Aproksimasi Integral Fungsi dengan Metode Numerik\n");
+      printf("4. Keluar\n\n");
+      printf("Pilih nomor menu: ");
+      scanf("%d",&num_menu);
+      system("cls");
+
+      switch(num_menu){
+            case 1:
+                  printf("\n\nAPROKSIMASI FUNGSI DENGAN METODE NUMERIK\n");
+                  printf("Metode yang digunakan pada aproksimasi fungsi adalah metode Interpolasi Lagrange\n");
+                  printf("Ingin lanjut? (y/n): ");
+                  scanf("%s",&next);
+                  if(next == 'y'){
+                        system("cls");
+                        InterpolasiLagrange: 
+                        system("cls");
+                        LagrangeInterpolation(x, f_x, n, x_inter);
+
+                        printf("\nMau mengulang? (y/n): ");
+                        scanf(" %c", &stay);
+                        if(stay == 'y'){
+                              goto InterpolasiLagrange;
+                        } else {
+                              goto Utama;
+                        }
+                  } else {
+                        goto Utama;
+                  }
+                  
+                  break;
+            case 4:
+                  printf("Keluar dari program\n");
+                  exit(0);
+      }
       return 0;
 }
