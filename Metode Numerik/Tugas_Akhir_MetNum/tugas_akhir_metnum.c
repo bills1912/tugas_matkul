b85575f63932cab59e5add07c7d095f320f456ab
bills <221911069@stis.ac.id>
2022-06-25 21:19:08 +0700

    Added two, three, and five point of differentiation numeric and another changes

--- Metode Numerik/Tugas_Akhir_MetNum/tugas_akhir_metnum.c
+++ Metode Numerik/Tugas_Akhir_MetNum/tugas_akhir_metnum.c
@@ -61,6 +61,99 @@ float LagrangeInterpolation(float x[], float f_x[], int n, float x_interpolate)
       }
 }
 
+float DiferensiasiDuaTitik(float f_x[], float x[], int n, float x_differ) 
+{
+      printf("APROKSIMASI TURUNAN NUMERIK DENGAN DUA TITIK\n\n");
+      float h, selisihMaju, selisihMundur; 
+      int i, pilihan;
+
+      printf("Masukkan nilai h: ");
+      scanf("%f", &h);
+
+      printf("Masukan x yang akan diaproksimasi turunannya: ");
+      scanf("%f", &x_differ);
+
+      printf("Pilihan rumus: \n");
+      printf("1. Aproksimasi Turunan Dua Titik dengan Selisih Maju\n");
+      printf("2. Aproksimasi Turunan Dua Titik dengan Selisih Mundur\n");
+      printf("\nMasukan pilihan rumus: ");
+      scanf("%d", &pilihan);
+
+      switch(pilihan){
+            case 1:
+                  selisihMaju = (func(x_differ + h) - func(x_differ))/h;
+                  printf("Hasil aproksimasi turunan dari titik %0.3f dengan selisih maju adalah: %0.7f", x_differ, selisihMaju);
+                  break;
+            case 2:
+                  selisihMundur = (func(x_differ) - func(x_differ - h))/h;
+                  printf("Hasil aproksimasi turunan dari titik %0.3f dengan selisih mundur adalah: %0.7f", x_differ, selisihMundur);
+                  break;
+      }
+
+}
+
+float DiferensiasiTigaTitik(float f_x[], float x[], int n, float x_differ) 
+{
+      printf("APROKSIMASI TURUNAN NUMERIK DENGAN TIGA TITIK\n\n");
+      float h, diferTigaTitik; 
+      int i, pilihan;
+
+      printf("Masukkan nilai h: ");
+      scanf("%f", &h);
+
+      printf("Masukan x yang akan diaproksimasi turunannya: ");
+      scanf("%f", &x_differ);
+
+      printf("Pilihan rumus: \n");
+      printf("1. Aproksimasi Turunan Tiga Titik dengan Psi yang Berada di antara (x0 - h) s/d (x0 + h)\n");
+      printf("2. Aproksimasi Turunan Tiga Titik dengan Psi yang Berada di antara x0 s/d (x0+2h)\n");
+      printf("\nMasukan pilihan rumus: ");
+      scanf("%d", &pilihan);
+
+      switch(pilihan){
+            case 1:
+                  diferTigaTitik = (func(x_differ + h) - func(x_differ - h))/(2*h);
+                  printf("Hasil aproksimasi turunan dari titik %0.3f dengan psi yang berada di antara (x0 - h) s/d (x0 + h) adalah: %0.7f", x_differ, diferTigaTitik);
+                  break;
+            case 2:
+                  diferTigaTitik = (-3*func(x_differ) + 4*func(x_differ + h) - func(x_differ + 2*h))/(2*h);
+                  printf("Hasil aproksimasi turunan dari titik %0.3f dengan psi yang berada di antara x0 s/d (x0 + 2h) adalah: %0.7f", x_differ, diferTigaTitik);
+                  break;
+      }
+
+}
+
+float DiferensiasiLimaTitik(float f_x[], float x[], int n, float x_differ) 
+{
+      printf("APROKSIMASI TURUNAN NUMERIK DENGAN LIMA TITIK\n\n");
+      float h, diferLimaTitik; 
+      int i, pilihan;
+
+      printf("Masukkan nilai h: ");
+      scanf("%f", &h);
+
+      printf("Masukan x yang akan diaproksimasi turunannya: ");
+      scanf("%f", &x_differ);
+
+      printf("Pilihan rumus: \n");
+      printf("1. Aproksimasi Turunan Lima Titik dengan Psi yang Berada di antara (x0 - 2h) s/d (x0 + 2h)\n");
+      printf("2. Aproksimasi Turunan LIma Titik dengan Psi yang Berada di antara x0 s/d (x0 + 4h)\n");
+      printf("\nMasukan pilihan rumus: ");
+      scanf("%d", &pilihan);
+
+      switch(pilihan){
+            case 1:
+                  diferLimaTitik = (func(x_differ - 2*h) - 8*func(x_differ - h) + 8*func(x_differ + h) - func(x_differ + 2*h))/(12*h);
+                  printf("Hasil aproksimasi turunan dari titik %0.3f dengan psi yang berada di antara (x0 - 2h) s/d (x0 + 2h) adalah: %0.7f", x_differ, diferLimaTitik);
+                  break;
+            case 2:
+                  diferLimaTitik = (-25*func(x_differ) + 48*func(x_differ + h) - 36*func(x_differ + 2*h) + 16*func(x_differ + 3*h) - 3*func(x_differ + 4*h))/(12*h);
+                  printf("Hasil aproksimasi turunan dari titik %0.3f dengan psi yang berada di antara x0 s/d (x0 + 4h) adalah: %0.7f", x_differ, diferLimaTitik);
+                  break;
+      }
+
+}
+
 float NewtonForwardDerivative(float x[], int n, float x_diff)
 {
       float f[100][100], h, sum = 0.0, term, first_derivative;
@@ -195,7 +288,7 @@ float NumericIntegral(float a, float b, float err[])
 
 int main(){
       float x[100], f_x[100], x_inter, x_diff, a, b, err[3];
-      int n, num_menu;
+      int n, num_menu, pilihan, method;
       char next, stay;
 
       Utama:
@@ -214,7 +307,7 @@ int main(){
             case 1:
                   printf("APROKSIMASI FUNGSI DENGAN METODE NUMERIK\n");
                   printf("Metode yang digunakan pada aproksimasi fungsi adalah metode Interpolasi Lagrange\n");
-                  printf("Ingin lanjut? (y/n): ");
+                  printf("\nIngin lanjut? (y/n): ");
                   scanf("%s",&next);
                   if(next == 'y'){
                         system("cls");
@@ -238,21 +331,97 @@ int main(){
             case 2:
                   printf("APROKSIMASI TURUNAN FUNGSI DENGAN METODE NUMERIK\n");
                   printf("Metode yang digunakan untuk mengestimasi turunan dari suatu titik kolokasi yaitu\nmenggunakan metode Newton Forward\n");
-                  printf("Ingin lanjut? (y/n): ");
+                  printf("\nIngin lanjut? (y/n): ");
                   scanf("%s",&next);
                   if(next == 'y'){
                         system("cls");
-                        TurunanNumerik: 
+                        TurunanNumerik:
                         system("cls");
-                        NewtonForwardDerivative(x, n, x_diff);
-                        
-                        printf("\nMau mengulang? (y/n): ");
-                        scanf(" %c", &stay);
-                        if(stay == 'y'){
-                              goto TurunanNumerik;
-                        } else {
-                              goto Utama;
-                        }
+                        printf("PILIH JENIS APROKSIMASI\n");
+                        printf("1. Aproksimasi Turunan dengan Fungsi yang Diketahui\n");
+                        printf("2. Aproksimasi Turunan dengan Fungsi yang Tidak Diketahui\n");
+                        printf("3. Kembali ke Menu Utama\n");
+                        printf("\nPilih nomor menu: ");
+                        scanf("%d",&pilihan);
+                        system("cls");
+                        switch(pilihan){
+                              case 1:
+                                    system("cls");
+                                    FungsiDiketahui:
+                                    system("cls");
+                                    printf("APROKSIMASI TURUNAN NUMERIK DENGAN FUNGSI YANG DIKETAHUI\n");
+                                    printf("\nPilih Metode yang diinginkan:\n");
+                                    printf("1. Aproksimasi Turunan Numerik dengan Dua Titik\n");
+                                    printf("2. Aproksimasi Turunan Numerik dengan Tiga Titik\n");
+                                    printf("3. Aproksimasi Turunan Numerik dengan Lima Titik\n");
+                                    printf("4. Kembali ke Menu Sebelumnya\n");
+                                    printf("\nPilih nomor menu: ");
+                                    scanf("%d",&method);
+                                    system("cls");
+                                    switch(method){
+                                          case 1:
+                                                system("cls");
+                                                TurunanNumerikDuaTitik:
+                                                system("cls");
+                                                DiferensiasiDuaTitik(f_x, x, n, x_diff);
+                                                printf("\nMau mengulang? (y/n): ");
+                                                scanf(" %c", &stay);
+                                                if(stay == 'y'){
+                                                      goto TurunanNumerikDuaTitik;
+                                                } else {
+                                                      goto FungsiDiketahui;
+                                                }
+                                                break;
+                                          case 2:
+                                                system("cls");
+                                                TurunanNumerikTigaTitik:
+                                                system("cls");
+                                                DiferensiasiTigaTitik(f_x, x, n, x_diff);
+                                                printf("\nMau mengulang? (y/n): ");
+                                                scanf(" %c", &stay);
+                                                if(stay == 'y'){
+                                                      goto TurunanNumerikTigaTitik;
+                                                } else {
+                                                      goto FungsiDiketahui;
+                                                }
+                                                break;
+                                          case 3:
+                                                system("cls");
+                                                TurunanNumerikLimaTitik:
+                                                system("cls");
+                                                DiferensiasiLimaTitik(f_x, x, n, x_diff);
+                                                printf("\nMau mengulang? (y/n): ");
+                                                scanf(" %c", &stay);
+                                                if(stay == 'y'){
+                                                      goto TurunanNumerikLimaTitik;
+                                                } else {
+                                                      goto FungsiDiketahui;
+                                                }
+                                                break;
+                                          case 4:
+                                                goto TurunanNumerik;
+                                                break;
+                                    }
+                                    break;
+                              case 2:
+                                    system("cls");
+                                    FungsiTidakDiketahui:
+                                    system("cls");
+                                    printf("APROKSIMASI TURUNAN NUMERIK DENGAN FUNGSI YANG DIKETAHUI\n");
+                                    printf("Pada metode ini, kita menggunakan metode Newton Forward\n");
+                                    NewtonForwardDerivative(x, n, x_diff);
+                                    printf("\nMau mengulang? (y/n): ");
+                                    scanf(" %c", &stay);
+                                    if(stay == 'y'){
+                                          goto FungsiTidakDiketahui;
+                                    } else {
+                                          goto TurunanNumerik;
+                                    }
+                                    break;
+                              case 3:
+                                    goto Utama;
+                                    break;
+                        } 
                   } else {
                         goto Utama;
                   }
@@ -261,7 +430,7 @@ int main(){
             case 3:
                   printf("APROKSIMASI INTEGRAL FUNGSI DENGAN METODE NUMERIK\n");
                   printf("Metode yang digunakan untuk menghitung integral dari suatu fungsi adalah metode MidPoint, Trapezoidal, dan Simpson 1/3\n");
-                  printf("Ingin lanjut? (y/n): ");
+                  printf("\nIngin lanjut? (y/n): ");
                   scanf("%s",&next);
                   if(next == 'y'){
                         system("cls");
